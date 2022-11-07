82e9f19388284a226376c42ad271d82255db96a7
Alessandro Sartor <alessandro.sartor.1999@gmail.com>
2022-11-07 15:28:02 +0100

    Added title

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,32 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppJumboVue from './components/AppJumbo.vue';
+
+export default {
+  data(){
+    return{
+      title: "Ciao io sono Uffa",
+    }
+  },
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header>
+    <h1>{{title}}</h1>
+  </header>
+  
+  <AppJumboVue />
+  
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+
+header{
+  width: 100%;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+header h1{
+  text-align: center;
+  font-family: sans-serif;
+  color: antiquewhite;
 }
 </style>
---
+++ src/components/AppJumbo.vue
@@ -0,0 +1,33 @@
+<script>
+export default{
+    data(){
+        return{
+            imgName: 'uffa.JPG'
+        }
+    },
+    methods:{
+        getImagePath(){
+            return new URL(`../assets/img/(this.imgName)`, import.meta.utl).href;
+        }
+    }
+}
+</script>
+
+<template>
+<div class="jumbo">
+    <img :src="getImagePath()" alt="Uffa">
+</div>
+
+</template>
+
+<style>
+.jumbo{
+  width: 100%;
+  height: 50vh;
+}
+.jumbo img{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+</style>
